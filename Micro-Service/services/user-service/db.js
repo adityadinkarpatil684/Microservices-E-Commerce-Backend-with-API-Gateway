64ffddf7c7e1c5df8e0a7d9d3adb65f8987e8d6f
adityadinkarpatil684 <148608206+adityadinkarpatil684@users.noreply.github.com>
2026-06-20 14:50:35 +0530

    Update db.js

--- Micro-Service/services/user-service/db.js
+++ Micro-Service/services/user-service/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "advap",
+    password: "",
     database: "user_db",
     waitForConnections: true,
     connectionLimit: 10
